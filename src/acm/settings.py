683146b0b722689f8e21353d900ee93e135cea12
Nican <nican@LNican.(none)>
2012-11-06 16:57:14 -0500

    Added missing files from server. Unkown changes.

--- src/acm/settings.py
+++ src/acm/settings.py
@@ -4,7 +4,8 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Ian Naval', 'ianaval@wpi.edu'),
+    ('Henrique Polido', 'nican@wpi.edu'),
 )
 
 MANAGERS = ADMINS
@@ -27,7 +28,7 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/New_York'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -59,7 +60,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/var/www/acm.wpi.edu/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -119,9 +120,10 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
+    'core',
 )
 
 # A sample logging configuration. The only tangible logging
---
+++ src/core/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls.defaults import patterns, url
+from django.views.generic import TemplateView
+
+from core.views import *
+
+urlpatterns = patterns('core.views',
+    url(r'^$', TemplateView.as_view(template_name='core/index.html')),
+)
