180492182746c4e4a7b31f326f5ea908ed1495ea
Nican <nican@LNican.(none)>
2012-11-07 15:58:46 -0500

    Added members page, and generic info in the about page.

--- src/core/urls.py
+++ src/core/urls.py
@@ -5,5 +5,8 @@ from core.views import *
 
 urlpatterns = patterns('core.views',
     url(r'^$', TemplateView.as_view(template_name='core/index.html')),
-    url(r'^join$', TemplateView.as_view(template_name='core/join.html'))
+    url(r'^join$', TemplateView.as_view(template_name='core/join.html')),
+    url(r'^about$', TemplateView.as_view(template_name='core/about.html')),
+    url(r'^contact$', TemplateView.as_view(template_name='core/contact.html')),
+    url(r'^members$', 'members'),
 )
--- src/core/views.py
+++ src/core/views.py
@@ -1,1 +1,48 @@
 # Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
+ACMOfficers = [
+	{
+		'year' :'2011-2012',
+		'members': [{
+			'name': "Henrique Polido",
+			'position': "President",
+			'photo': "2011/henrique.png"
+		},{
+			'name': "Alex Karp",
+			'position': "Vice-President",
+			'photo': "2011/alex.png"
+		},{
+			'name': "Marc Green",
+			'position': "Treasure",
+			'photo': "2011/marc.png"
+		},{
+			'name': "Taymon Beal",
+			'position': "?",
+			'photo': "2011/taymon.png"
+		},{
+			'name': "Thinh Nguyen",
+			'position': "?",
+			'photo': "2011/thinh.png"
+		},{
+			'name': "Michael Checca",
+			'position': "?",
+			'photo': "2011/michael.png"
+		}]
+	},
+	{
+		'year' :'2010-2011',
+		'members': [{
+			'name': "Nate Thorn",
+			'position': "President",
+			'photo': "2010/nate.png"
+		}]
+	}
+]
+
+
+
+def members(request):
+	return render_to_response('core/members.html', {'officers': ACMOfficers}, context_instance=RequestContext(request))
